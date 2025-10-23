ded104f061a4e3944f238aef12c4016514bc9cb2
Sanjaithun <sanjaithunhema@gmail.com>
2025-10-23 22:40:20 +0530

    Encapthulations 2

--- dsatopics/OOPS/Encpaculation.java
+++ dsatopics/OOPS/Encpaculation.java
@@ -1,7 +1,28 @@
 package dsatopics.OOPS;
 
 public class Encpaculation {
+    private String name;
+    private String role;
+    public Encpaculation(String name , String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    String getName(){
+        return name;
+    }
+    String setName(String names){
+        this.name = names;
+        return "The Updated Name is: " + this.name;
+    }
+    String getRole(){
+        return this.role;
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Encpaculation obj = new Encpaculation("Sanjaithun", "Admin");
+        System.out.println(obj.getName());
+        System.out.println(obj.setName("Pramoth"));
+        System.out.println(obj.getRole());
     }
 } 
